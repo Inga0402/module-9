30e04f7a7fc1af10697e3c9d49d2b7ab84dec1d2
Inga Grigorieva <ingagrigorieva0402@gmail.com>
2020-12-05 11:01:01 +0200

    Added webpack

---
+++ src/a.js
@@ -0,0 +1,7 @@
+import b from "./b";
+
+console.log(b);
+
+const a = 5;
+
+export default a;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
-const express = require('express');
-const app = express()
+import value from "./a";
+
+console.log(value);
+
+// const express = require('express');
+// const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
  
-app.listen(3000, () => {
-    console.log("App is running on port 3000");
-});
+// app.listen(3000, () => {
+//     console.log("App is running on port 3000");
+// });
 
  
- const validator = require('validator');
+//  const validator = require('validator');
 
  
- const message = "Node js is cool!";
+//  const message = "Node js is cool!";
 
-console.log (message);
+// console.log (message);
 
-const validateEmail = email => {
-    return validator.isEmail(email);
-  };
-  console.log(
-    'Is mango@mail.com a valid email?: ',
-    validateEmail('mango@mail.com'),
-  );
+// const validateEmail = email => {
+//     return validator.isEmail(email);
+//   };
+//   console.log(
+//     'Is mango@mail.com a valid email?: ',
+//     validateEmail('mango@mail.com'),
+//   );
   
-  console.log(
-    'Is Mangozedog.com a valid email?: ',
-    validateEmail('Mangozedog.com'),
-  );
+//   console.log(
+//     'Is Mangozedog.com a valid email?: ',
+//     validateEmail('Mangozedog.com'),
+//   );
+
+//   class User{
+//     consrtuctor(name){
+//       this._name = name;
+//     }
+
+//     get name(){
+//       return this._name;
+//     }
+//   }
+
+//   const mango = new User("Mango");
+
+//   console.log(mango.name);
